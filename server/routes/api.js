56449a68caa82ee44b98aa64348e66ff42bd5f4e
leandro <carolino.leandro@gmail.com>
2020-11-02 12:15:54 +0100

    checking login

--- server/routes/api.js
+++ server/routes/api.js
@@ -4,6 +4,7 @@ const User = require('../models/user')
 
 const mongoose = require('mongoose')
 const e = require('express')
+const user = require('../models/user')
 const db = 'mongodb://leandro:Wrestling26@ds041484.mlab.com:41484/eventsdb'
 
 
@@ -29,6 +30,25 @@ router.post('/register', (req, res) => {
             console.log(error)
         } else {
             res.status(200).send(registeredUser)
+        }
+    })
+})
+
+router.post('/login', (req, res) => {
+    let userData = req.body
+
+    User.findOne({email: userData.email}, (error, user) => {
+        if(error) {
+            console.log(error)
+        } else {
+            if(!user){
+                res.status(401).send('invalid email')
+            } else 
+            if( user.password !== userData.password){
+                res.status(401).send('Invalid password')
+            } else {
+                res.status(200).send(user)
+            }
         }
     })
 })
